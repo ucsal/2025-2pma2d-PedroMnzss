806a176c0755675cdadade1e30285e6a563bf348
Pedro Vilas Boas Menezes <pedrovbm2006@gmail.com>
2025-08-28 19:08:08 -0300

    Implementação da abstração MailSender e refatoração da classe EmailNotifier

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import br.com.mariojp.solid.dip.*;
 public class Main {
 	public static void main(String[] args) {
 		System.setProperty("DRY_RUN", "true"); // desejável não chamar SMTP
-		var notifier = new EmailNotifier();
+		var notifier = new EmailNotifier(new SmtpClient());
 		// Estado inicial: vai lançar IllegalStateException (SMTP indisponível)
 		notifier.welcome(new User("Ana", "ana@example.com"));
 		System.out.println("Email enviado!");
